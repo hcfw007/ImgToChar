4cd4370d587148d38053b58e6acd43e47bc38f5f
hcfw007 <brotherstyx@gmail.com>
2020-06-02 15:53:03 +0800

    修改图片载入方式并转化为canvas

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-let img = document.getElementById('origin')
+const originSrc = './imgs/bunny.jpg'
+
+let img = new Image()
+img.src = originSrc
+img.onload = function() {
+  let h = img.naturalHeight
+  let w = img.naturalWidth
+  canvas = document.getElementById('origin')
+  if (h > w) {
+    w = w / h * 400
+    h = 400
+  } else {
+    h = h / w * 400
+    w = 400
+  }
+  canvas.width = w
+  canvas.height = h
+  let context = canvas.getContext('2d')
+  context.drawImage(img, 0, 0)
+}
